f8f22bf1de1ddb7011d25e45fbd6203987ef44ed
Armin <becherarmin@gmail.com>
2019-01-07 19:29:14 +0100

    Camera Ortho update

--- pysg/camera.py
+++ pysg/camera.py
@@ -48,3 +48,38 @@ class PerspectiveCamera(Camera):
             0.0, y, 0.0, 0.0,
             0.0, 0.0, -1.0, -1.0,
             0.0, 0.0, z, 0.0])
+
+
+class OrthographicCamera(Camera):
+    """Uses square as projection matrix"""
+
+    def __init__(self, *, left, right, top, bottom, near, far):
+        """
+        Args:
+            left (float):  Camera volume left (usually -screen_width/2).
+            right (float):  Camera volume right (usually screen_width/2).
+            top (float):  Camera volume top (usually screen_height/2).
+            bottom (float):  Camera volume bottom (usually -screen_height/2)..
+            near (float): Camera frustum near plane. Everything closer will be culled.
+            far (float): Camera frustum far plane. Everything farther away will be culled.
+
+        """
+        super().__init__()
+        self.bottom = bottom
+        self.top = top
+        self.right = right
+        self.left = left
+        self._near = near
+        self._far = far
+        self.projection_matrix = self.compute_projection_matrix()
+
+    def compute_projection_matrix(self):
+        z = (-2.0 * self._near * self._far) / (self._far - self._near)
+        y = 1.0 / math.tan(self._fov * math.pi / 360)
+        x = y / self._aspect
+
+        return np.array([
+            x, 0.0, 0.0, 0.0,
+            0.0, y, 0.0, 0.0,
+            0.0, 0.0, -1.0, -1.0,
+            0.0, 0.0, z, 0.0])
