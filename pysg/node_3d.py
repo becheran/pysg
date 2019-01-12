f89038d77ac97b781a144ad55f9317814945d479
Armin Becher <becherarmin@gmail.com>
2019-01-12 13:19:07 +0100

    Added functions for transform access in node3d

--- pysg/node_3d.py
+++ pysg/node_3d.py
@@ -2,7 +2,7 @@
 """The base of all objects which are displayed in the scene.
 """
 
-from pyrr import Matrix44, Vector3
+from pyrr import Matrix44, Vector3, Quaternion
 
 
 class Node3D:
@@ -10,14 +10,61 @@ class Node3D:
 
     def __init__(self, name: str = "New Node"):
         """ Scene is the base node. All other nodes need to be added to the scene.
+
+        Args:
+            name: Name for string representation.
         """
         self.children = list()
         self.parent = None
         self.name = name
 
-        self.matrix_world = Matrix44.identity()
-        self.matrix = Matrix44.identity()
-        self.eulerAngles = Vector3()
+        self._local_position = Vector3()
+        self._world_position = Vector3()
+
+        self._scale = Vector3()
+        # TODO global scale?
+
+        self._local_quaternion = Quaternion()
+        self._world_quaternion = Quaternion()
+        self._local_eulerAngles = Vector3()
+        self._world_eulerAngles = Vector3()
+        self._world_matrix = Matrix44.identity()
+        self._local_matrix = Matrix44.identity()
+        # TODO Rotation as matrix33
+
+        self.__matrix_needs_update = False
+
+    @property
+    def local_position(self):
+        return self._local_position
+
+    @local_position.setter
+    def local_position(self, local_position):
+        self.__matrix_needs_update = True
+        self._local_position = local_position
+
+    @property
+    def world_position(self):
+        return self._world_position
+
+    @world_position.setter
+    def world_position(self, world_position):
+        self.__matrix_needs_update = True
+        # TODO UPDATE CHILDREN
+        self._world_position = world_position
+
+    @property
+    def local_matrix(self):
+        if self.__matrix_needs_update:
+            self.__matrix_needs_update = False
+            # TODO
+        return self._local_matrix
+
+    @property
+    def world_matrix(self):
+        if self.__matrix_needs_update:
+            self.__matrix_needs_update = False
+        return self._world_matrix
 
     def __repr__(self):
         return "Node(%s)" % self.name
@@ -42,14 +89,14 @@ class Node3D:
     def update_world_matrix(self) -> None:
         """ Updates the world matrix for a given node in the render scene graph."""
         if self.parent is None:
-            self.matrix_world = self.matrix
+            self._world_matrix = self._local_matrix
         else:
-            self.matrix_world = self.matrix_world * self.matrix
+            self._world_matrix = self.parent.world_matrix * self._local_matrix
 
-        # TODO execute in multiple worker threads?
         for child in self.children:
             child.update_world_matrix()
 
+    # TODO Add check function as parameter
     def get_leaf_nodes(self) -> list:
         """ Recursively goes over all node children and returns Node3D list.
 
