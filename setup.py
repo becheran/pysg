7ad89ce4c5dc75d23bccd881214d6a9d77632edc
becheran <becherarmin@gmail.com>
2019-01-16 11:42:58 +0100

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,2 +1,23 @@
-# Python 3.6 required minimum due to the use of static type checking
-python_requires = '>=3.6'
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pysg",
+    version="0.0.1",
+    author="Armin Becher",
+    author_email="becherarmin@gmail.com",
+    description="Simple and lightweight 3D render scene graph for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/becheran/pysg",
+    packages=setuptools.find_packages(),
+	# Python 3.6 required minimum due to the use of static type checking
+	python_requires = '>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
