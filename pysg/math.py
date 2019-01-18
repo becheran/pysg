Fixed euler <-> quaternion conversion

pysg/math.py
@@ -5,6 +5,7 @@ from typing import List
 import numpy as np
 import pyrr
 from pyrr import Matrix44, Quaternion, Vector3
+from pyrr.utils import parameters_as_numpy_arrays
 
 
 def is_angle(angle, *, in_degrees, allow_negative, limit_to_circle):
@@ -136,10 +137,10 @@ def quaternion_are_equal(q1: Quaternion, q2: Quaternion, epsilon: float = 1e-12)
 
 
 # TODO push to pyrr
-#TODO add rotation order
+# TODO add rotation order
 # From http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToAngle/
 def quaternion_to_euler_angles(quaternion: Quaternion) -> Vector3:
-    """ Converts quaternion to euler angles in YXZ order.
+    """ Converts quaternion to euler angles in degrees and YZX order.
 
     First heading, then attitude, then bank. See also:
     http://www.euclideanspace.com/maths/geometry/rotations/euler/index.htm
@@ -149,20 +150,57 @@ def quaternion_to_euler_angles(quaternion: Quaternion) -> Vector3:
     Returns:
         Vector3: Vector3 representation of quaternion
     """
-    t = quaternion.x * quaternion.y + quaternion.z * quaternion.w
-    if t > 0.499:  # singularity at north pole
-        x = 2 * np.math.atan2(quaternion.x, quaternion.w)
-        y = np.math.pi / 2
-        z = 0
-    elif t < -0.499:  # singularity at south pole
-        x = -2 * np.math.atan2(quaternion.x, quaternion.w)
-        y = - np.math.pi / 2
-        z = 0
+    sqw = quaternion.w * quaternion.w
+    sqx = quaternion.x * quaternion.x
+    sqy = quaternion.y * quaternion.y
+    sqz = quaternion.z * quaternion.z
+    unit = sqx + sqy + sqz + sqw  # if normalised is one, otherwise is correction factor
+    test = quaternion.x * quaternion.y + quaternion.z * quaternion.w
+    if test > 0.499 * unit:  # singularity at north pole
+        heading = 2 * math.atan2(quaternion.x, quaternion.w)
+        attitude = math.pi / 2.
+        bank = 0.0
+    elif test < -0.499 * unit:  # singularity at south pole
+        heading = -2 * math.atan2(quaternion.x, quaternion.w)
+        attitude = -math.pi / 2.
+        bank = 0
     else:
-        sx = quaternion.x * quaternion.x
-        sy = quaternion.y * quaternion.y
-        sz = quaternion.z * quaternion.z
-        x = np.math.atan2(2 * quaternion.y * quaternion.w - 2 * quaternion.x * quaternion.z, 1 - 2 * sy - 2 * sz)
-        y = np.math.asin(2 * quaternion.x * quaternion.y + 2 * quaternion.z * quaternion.w)
-        z = np.math.atan2(2 * quaternion.x * quaternion.w - 2 * quaternion.y * quaternion.z, 1 - 2 * sx - 2 * sz)
-    return Vector3([x, y, z])
+        heading = math.atan2(2 * quaternion.y * quaternion.w - 2 * quaternion.x * quaternion.z, sqx - sqy - sqz + sqw)
+        attitude = math.asin(2 * test / unit)
+        bank = math.atan2(2 * quaternion.x * quaternion.w - 2 * quaternion.y * quaternion.z, -sqx + sqy - sqz + sqw)
+    return Vector3(np.rad2deg([bank, heading, attitude]))
+
+
+# TODO push to pyrr?
+@parameters_as_numpy_arrays('eulers')
+def euler_angles_to_quaternion(eulers, dtype=None):
+    """Creates a quaternion from a set of Euler angles.
+
+    Rotation order for euler angles is YZX.
+
+    See: http://www.euclideanspace.com/maths/geometry/rotations/conversions/eulerToQuaternion/index.htm
+
+    """
+    dtype = dtype or eulers.dtype
+
+    heading_half = eulers[1] / 2.
+    attitude_half = eulers[2] / 2.
+    bank_half = eulers[0] / 2.
+
+    c1 = np.math.cos(heading_half)
+    s1 = np.math.sin(heading_half)
+    c2 = np.math.cos(attitude_half)
+    s2 = np.math.sin(attitude_half)
+    c3 = np.math.cos(bank_half)
+    s3 = np.math.sin(bank_half)
+    c1c2 = c1 * c2
+    s1s2 = s1 * s2
+    return Quaternion(
+        [
+            c1c2 * s3 + s1s2 * c3,
+            s1 * c2 * c3 + c1 * s2 * s3,
+            c1 * s2 * c3 - s1 * c2 * s3,
+            c1c2 * c3 - s1s2 * s3,
+        ],
+        dtype=dtype
+    )

tests/test_math.py
@@ -2,12 +2,13 @@ from unittest import TestCase
 
 import numpy as np
 import pyrr
-from pyrr import Quaternion
+from pyrr import Quaternion, Vector3
 
-from pysg.math import ray_intersect_sphere, quaternion_are_equal
+from pysg.math import ray_intersect_sphere, quaternion_are_equal, euler_angles_to_quaternion, quaternion_to_euler_angles
+from pysg.testing import CustomAssertions
 
 
-class TestMath(TestCase):
+class TestMath(TestCase, CustomAssertions):
     def test_ray_intersect_sphere_no_solution_1(self):
         ray = pyrr.ray.create([0, 2, 0], [1, 0, 0])
         sphere = pyrr.sphere.create([0, 0, 0], 1)
@@ -51,3 +52,48 @@ class TestMath(TestCase):
 
     def test_quaternion_are_equal_3(self):
         self.assertTrue(quaternion_are_equal(Quaternion([1, 0, 0, 0]), Quaternion([1, 0, 0, 0])))
+
+    def test_euler_to_quaternion_1(self):
+        v = np.radians(Vector3([0, 0, 0]))
+        self.assertQuaternionAreEqual(euler_angles_to_quaternion(v), (Quaternion([0, 0, 0, 1])))
+        v = np.radians([0, 0, 180])
+        self.assertQuaternionAreEqual(euler_angles_to_quaternion(v), (Quaternion([0, 0, 1, 0])))
+        v = np.radians([0, 180, 0])
+        self.assertQuaternionAreEqual(euler_angles_to_quaternion(v), (Quaternion([0, 1, 0, 0])))
+        v = np.radians([180, 0, 0])
+        self.assertQuaternionAreEqual(euler_angles_to_quaternion(v), (Quaternion([1, 0, 0, 0])))
+
+    def test_euler_to_quaternion_2(self):
+        v = np.radians([0, 0, 360])
+        self.assertQuaternionAreEqual(euler_angles_to_quaternion(v), (Quaternion([0, 0, 0, 1])))
+
+    def test_euler_to_quaternion_3(self):
+        v = np.radians(Vector3([90, 70, 45]))
+        e = euler_angles_to_quaternion(v)
+        self.assertQuaternionAreEqual(e, Quaternion([0.6903455, 0.5963678, -0.1530459, 0.3799282]), epsilon=1e-5)
+
+    def test_quaternion_to_euler_1(self):
+        q = Quaternion([0, 0, 0, 1])
+        e = quaternion_to_euler_angles(q)
+        np.testing.assert_almost_equal(np.array(e), np.array([0., 0., 0.]))
+
+    def test_quaternion_to_euler_2(self):
+        q = Quaternion([0, 0, 1, 0])
+        e = quaternion_to_euler_angles(q)
+        # Rotation [180, 180, 0] and [0,0,180] are the same thing.
+        np.testing.assert_almost_equal(np.array(e), np.array([180., 180., 0.]))
+
+    def test_quaternion_to_euler_3(self):
+        q = Quaternion([0, 1, 0, 0])
+        e = quaternion_to_euler_angles(q)
+        np.testing.assert_almost_equal(np.array(e), np.array([0., 180., 0.]))
+
+    def test_quaternion_to_euler_4(self):
+        q = Quaternion([1, 0, 0, 0])
+        e = quaternion_to_euler_angles(q)
+        np.testing.assert_almost_equal(np.array(e), np.array([180., 0., 0.]))
+
+    def test_quaternion_to_euler_5(self):
+        q = Quaternion([0.3711136, 0.0742227, 0.519559, 0.7660444])
+        e = quaternion_to_euler_angles(q)
+        np.testing.assert_almost_equal(np.array(e), np.array([69.4060045, -31.1935729, 58.3315948]), decimal=5)

tests/test_util.py
@@ -1,12 +1,12 @@
 from unittest import TestCase
 
+import numpy as np
 from pyrr import Vector3, Quaternion
 
-from pysg.testing import CustomAssertions
 from pysg.error import PyrrTypeError
-from pysg.math import quaternion_are_equal
+from pysg.math import euler_angles_to_quaternion
+from pysg.testing import CustomAssertions
 from pysg.util import pyrr_type_checker, parameters_as_angles_deg_to_rad
-import numpy as np
 
 
 class TestUtil(TestCase, CustomAssertions):
@@ -35,16 +35,3 @@ class TestUtil(TestCase, CustomAssertions):
             np.testing.assert_almost_equal(np.array(angle), np.array(result), decimal=5)
 
         test(Vector3([45, 0, -280]), Vector3([0.785398, 0, -4.88692]))
-
-    def test_quaternion_to_euler_angles(self):
-        v = np.radians(Vector3([0, 0, 0]))
-        self.assertQuaternionAreEqual(Quaternion.from_eulers(v), (Quaternion([0, 0, 0, 1])))
-        v = np.radians([0, 0, 180])
-        self.assertQuaternionAreEqual(Quaternion.from_eulers(v), (Quaternion([0, 0, 1, 0])))
-        v = np.radians([0, 0, 360])
-        self.assertQuaternionAreEqual(Quaternion.from_eulers(v), (Quaternion([0, 0, 0, 1])))
-        v = np.radians([90, 70, 45])
-        e = Quaternion.from_eulers(v)  # [-0.69034553 -0.15304592  0.15304592  0.69034553]
-        print(e)
-        self.assertQuaternionAreEqual(Quaternion.from_eulers(v),
-                                      Quaternion([0.69034553, 0.15304592, -0.15304592, 0.69034553]))