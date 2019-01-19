1b57a16cbd1a2bde3c0dc29fb016945c621554bb
Armin Becher <becherarmin@gmail.com>
2019-01-19 11:30:14 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import setuptools
+import os
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+    
+if os.environ.get('CI_COMMIT_TAG'):
+    version = os.environ['CI_COMMIT_TAG']
+else:
+    version = os.environ['CI_JOB_ID']
 
 setuptools.setup(
     name="pysg",
-    version="0.0.1",
+    version=version,
     author="Armin Becher",
     author_email="becherarmin@gmail.com",
     description="Simple and lightweight 3D render scene graph for python",
@@ -15,9 +21,11 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     # Python 3.6 required minimum due to the use of static type checking
     python_requires='>=3.6',
+    license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    zip_safe=False
 )
