55a2e9bf379c81c6afaeebab1bbbf0fca1ef045d
Armin <becherarmin@gmail.com>
2019-02-10 18:22:50 +0100

    Added function export in init

--- pysg/__init__.py
+++ pysg/__init__.py
@@ -1,3 +1,34 @@
+""" Export all modules needed to use pysg."""
 from __future__ import absolute_import
+
+# Camera
+from .camera import PerspectiveCamera
+from .camera import OrthographicCamera
+
+# Light
+from .light import PointLight
+
+# Node3D
+from .node_3d import Node3D
+
+# 3D Objects
+from .object_3d import CircleObject3D
+from .object_3d import CubeObject3D
+from .object_3d import CylinderObject3D
+from .object_3d import IcosahedronObject3D
+from .object_3d import PlaneObject3D
+from .object_3d import PyramidObject3D
+from .object_3d import TetrahedralObject3D
+from .object_3d import TriangleObject3D
+
+# Renderer
+from .renderer import GLRenderer
+from .renderer import HeadlessGLRenderer
+
+# Scene
+from .scene import Scene
+
+# Constants
+from . import constants
 
 from pysg.version import __version__
