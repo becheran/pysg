23290b928e8ce204e83e4f9e52c91813c1982be6
Armin <becherarmin@gmail.com>
2019-03-05 23:30:10 +0100

    Add name paramter to light object

--- pysg/light.py
+++ pysg/light.py
@@ -7,19 +7,19 @@ from pysg.node_3d import Node3D
 
 class Light(Node3D):
 
-    def __init__(self, color: tuple) -> 'Light':
+    def __init__(self, color: tuple, name: str) -> 'Light':
         """ Base class of all lights which can be added to the scene.
         """
-        super().__init__()
+        super().__init__(name)
         self.color = color
 
 
 class PointLight(Light):
 
-    def __init__(self, color: tuple) -> 'PointLight':
+    def __init__(self, color: tuple, name: str) -> 'PointLight':
         """ Point light emits light in all directions from a single point.
 
         Args:
             color: The light intensity of light source.
         """
-        super().__init__(color)
+        super().__init__(color, name)
